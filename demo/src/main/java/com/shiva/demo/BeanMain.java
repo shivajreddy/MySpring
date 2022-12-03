e9da2aa88ba202ceb4fd32f4776037385d8ab916
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-02 20:38:45 -0500

    get all beans

--- demo/src/main/java/com/shiva/demo/BeanMain.java
+++ demo/src/main/java/com/shiva/demo/BeanMain.java
@@ -11,21 +11,19 @@ import java.util.Arrays;
 public class BeanMain {
     public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+        // var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
         // System.out.println(context.getBean("address-1"));
         // System.out.println(context.getBean(Address.class));
         // System.out.println();
 
-        var context2 = new AnnotationConfigApplicationContext(MyConfiguration.class);
+        var context = new AnnotationConfigApplicationContext(MyConfiguration.class);
 
         // System.out.println(context.getBean("bean_name"));
         // System.out.println(context.getBean("bean_age"));
 
-        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-
-
+        // System.out.println(context.getBean(String.class));
+        System.out.println(context.getBean("bean_params"));
 
     }
 }
-
--- demo/src/main/java/com/shiva/demo/game/MyConfiguration.java
+++ demo/src/main/java/com/shiva/demo/game/MyConfiguration.java
@@ -1,7 +1,9 @@
 package com.shiva.demo.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
 record Person(String name, int age) {
@@ -21,6 +23,17 @@ public class MyConfiguration {
     @Bean(name = "bean_age")
     public int age(){
         return 27;
+    }
+
+    @Bean(name = "bean_address")
+    @Qualifier("bean_address")
+    public String address(){
+        return "4506 monument ave";
+    }
+
+    @Bean (name ="bean_params")
+    public String bean_using_params(String bean_name, @Qualifier("bean_address") String address){
+        return bean_name + " : " + address;
     }
 
 }
