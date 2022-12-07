b9144279f73848915ba770394a0dbf1ee9154b7f
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-07 16:31:12 -0500

    springboot3 project for webapp

---
+++ spring-webapp/src/main/java/com/shiva/webapp/springwebapp/SpringWebappApplication.java
@@ -0,0 +1,13 @@
+package com.shiva.webapp.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebappApplication.class, args);
+	}
+
+}
---
+++ spring-webapp/src/test/java/com/shiva/webapp/springwebapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiva.webapp.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
