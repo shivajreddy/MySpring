f1530138b6f5a04ff6d6e64349364eb0bb09df3d
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-08 06:20:43 -0500

    restart app

---
+++ spring-webapp/src/main/java/com/shiva/webapp/springwebapp/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.shiva.webapp.springwebapp.hello;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String sayHello() {
+        return "sayHello";
+    }
+}
