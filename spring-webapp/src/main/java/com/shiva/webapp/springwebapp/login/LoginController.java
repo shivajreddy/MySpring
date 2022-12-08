10e039b8c19c8fbe2cee7ce323f73f4b94536fd0
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-08 15:39:26 -0500

    @ModelAttribute

--- spring-webapp/src/main/java/com/shiva/webapp/springwebapp/login/LoginController.java
+++ spring-webapp/src/main/java/com/shiva/webapp/springwebapp/login/LoginController.java
@@ -1,19 +1,30 @@
 package com.shiva.webapp.springwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String loginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
-        model.put("firstName", name);
+    /* using a logger */
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage() {
         return "loginPage";
     }
-}
 
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String welcomePage(@ModelAttribute LoginForm loginForm) {
+        System.out.println("Username: " + loginForm.getUsername());
+        System.out.println("Password: " + loginForm.getPassword());
+        return "resultPage";
+    }
+
+}
 
---
+++ spring-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/resultPage.jsp
@@ -0,0 +1,6 @@
+<html>
+<head><title>Result</title></head>
+<body><h1>Result</h1>
+<h2>Given username: ${username}</h2>
+<h2>Given password: ${password}</h2></body>
+</html>
