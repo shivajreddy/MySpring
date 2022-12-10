9bcb2c291303fe465161de2acd775c81b733e61b
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-10 14:39:09 -0500

    RESTAPI - fresh project

---
+++ restful-web-services/src/main/java/com/shiva/webservices/restfulwebservices/helloWorld/Hello.java
@@ -0,0 +1,13 @@
+package com.shiva.webservices.restfulwebservices.helloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello world!";
+    }
+}
