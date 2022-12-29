b0f4e8d70eb58dc14bd4e11713e869960404b23e
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-29 11:34:32 -0500

    new project setup for blog-api2. Everything re-do until security

---
+++ blog-rest-api2/src/main/java/com/blog2/blogrestapi2/BlogRestApi2Application.java
@@ -0,0 +1,13 @@
+package com.blog2.blogrestapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRestApi2Application.class, args);
+	}
+
+}
---
+++ blog-rest-api2/src/test/java/com/blog2/blogrestapi2/BlogRestApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog2.blogrestapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
