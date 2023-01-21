58a39db744c55d424033f5abac61fec5dd1f5cbc
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-01-21 14:33:53 -0500

    endpoint authorizations ch6 video finished

--- ss-ch5-e1/src/main/java/com/shiva/ssch5e1/security/SecurityConfig.java
+++ ss-ch5-e1/src/main/java/com/shiva/ssch5e1/security/SecurityConfig.java
@@ -1,13 +1,29 @@
 package com.shiva.ssch5e1.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.access.intercept.RequestMatcherDelegatingAuthorizationManager;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
 
 @Configuration
 public class SecurityConfig {
@@ -16,20 +32,13 @@ public class SecurityConfig {
     public String secretKey;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthorizationManager<RequestAuthorizationContext> access) throws Exception {
 
         return http
                 .httpBasic().and()
 
                 // custom filter
                 //.addFilterBefore(new CustomFilter(secretKey), BasicAuthenticationFilter.class)
-
-                //.authorizeHttpRequests().anyRequest().authenticated().and() // end-point level authorization
-
-                .authorizeHttpRequests().requestMatchers("/admin").hasAuthority("write").and()
-
-                .authorizeHttpRequests()
-                .anyRequest().hasAuthority("read").and()
 
                 /*     authorize the following + matcher method + authorization rule
                  * Ex: authorizeHttpRequests()  .anyRequest()    .authenticated()
@@ -40,6 +49,12 @@ public class SecurityConfig {
                  * authorization rule -> .authenticated()
                  *                       .hasAuthority("some_authority")
                  */
+                //.authorizeHttpRequests().requestMatchers("/admin").hasAuthority("write").and()
+                //.authorizeHttpRequests()
+
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().access(new auth)
+                )
 
                 //.formLogin(Customizer.withDefaults())
                 .build();
---
+++ ss-ch6-e1/src/main/java/com/shiva/ssch6e1/SsCh6E1Application.java
@@ -0,0 +1,13 @@
+package com.shiva.ssch6e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsCh6E1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsCh6E1Application.class, args);
+    }
+
+}
